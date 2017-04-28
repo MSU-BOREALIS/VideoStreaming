33f2a361e257302d006ecda24b9956493c54f127
sabartheman <skylar4494@gmail.com>
2017-04-27 20:09:19 -0600

    added option for running on a single camera with a servo or an octaplexer system.

---
+++ fs-overlay/fs/home/pi/bin/python/single_servo/README.md
@@ -0,0 +1,7 @@
+To use this file input a value from 0 to 10 after calling the function
+
+Example:
+sudo python adjust_servo.py 5
+
+
+
---
+++ fs-overlay/fs/home/pi/bin/python/single_servo/adjust_servo.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import argparse
+import sys
+from time import sleep
+
+PWM_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PWM_PIN, GPIO.OUT)
+GPIO.setwarnings(False)
+pwm = GPIO.PWM(PWM_PIN, 500)
+
+
+#def setup():
+#	pwm.start(25)
+
+def main():
+	duty_cycle = 10
+	dataint = map(int, sys.argv[1:])
+	duty_cycle = (dataint[0] * 3) + 30
+	pwm.start(duty_cycle)
+	sleep(.5)
+
+main()
+GPIO.cleanup()
